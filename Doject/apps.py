6b4f9f1d8fe7eded2f726bc00aed381d9114cc6d
adsz24 <adamszym24@gmail.com>
2024-01-31 21:36:49 +0100

    Add project files.

---
+++ Doject/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DojectConfig(AppConfig):
+    name = 'Doject'
---
+++ Doject/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from datetime import datetime
+
+
+
+# Create your views here.
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'Doject/index.html',
+        {
+            'title': 'Home Page',
+            'year': datetime.now().year,
+        }
+    )
